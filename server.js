d2445622c024fc5b52122d6ccdda87f6476ba6ea
BorisNgd <bngd228@gmail.com>
2020-04-08 02:06:39 +0100

    bug fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿
 
 const server = require('./configuration/app')();
-const config = require(' ./configuration/config/config');
+const config = require('./configuration/config/config');
 const database = require('./configuration/database');
 
 //create the basic server setup
