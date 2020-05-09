6d662b0c5b5b5eeef60941efa240c3aa701082ac
BorisNgd <bngd228@gmail.com>
2020-05-09 09:34:49 +0100

    update server

--- configuration/database.js
+++ configuration/database.js
@@ -1,19 +1,19 @@
 ﻿let config = {
-   /*  database: {
+     database: {
         host: 'remotemysql.com',
         user: 'T7MR5Szqlh',
         password: 'gMoODmoKuU',
         database: 'T7MR5Szqlh',
         port: 3306
-    } */
+    } 
 
-    database: {
+  /*   database: {
         host: '127.0.0.1',
         user: 'root',
         password: '',
         database: 'logement',
         port: 3306
-    }
+    } */
 }
 
 module.exports = config; 
