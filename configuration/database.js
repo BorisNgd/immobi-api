d94470bec94e995af865d29e70ea052ff8dd0a1b
BorisNgd <bngd228@gmail.com>
2020-07-04 08:48:01 +0100

    Updated

--- configuration/database.js
+++ configuration/database.js
@@ -1,20 +1,20 @@
 ﻿ let config = {
      
-/*      database: {
+     database: {
         host: 'remotemysql.com',
         user: 'T7MR5Szqlh',
         password: 'gMoODmoKuU',
         database: 'T7MR5Szqlh',
         port: 3306
-    }   , */
+    }   , 
 
-         database: {
+ /*         database: {
         host: 'sql7.freemysqlhosting.net',
         user: 'sql7350790',
         password: 'gJdab8QTDZ',
         database: 'sql7350790',
         port: 3306
-    }   ,
+    }   , */
 
   /*      database: {
         host: '127.0.0.1',
